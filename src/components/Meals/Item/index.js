0e05e9ec450aeab018fb95b597916b0b248f4d39
basofiibnu <>
2022-02-09 11:34:42 +0700

    added cart form in meals item

--- src/components/Meals/Item/index.js
+++ src/components/Meals/Item/index.js
@@ -1,3 +1,4 @@
+import MealItemForm from './Form';
 import styles from './style.module.css';
 
 const MealsItem = (props) => {
@@ -10,7 +11,9 @@ const MealsItem = (props) => {
         <div className={styles.description}>{props.description}</div>
         <div className={styles.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm id={props.id} />
+      </div>
     </li>
   );
 };
