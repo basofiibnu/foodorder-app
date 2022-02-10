4c6ef9b5bc6c6abbd26c2d51fbac11d962716ce0
basofiibnu <>
2022-02-10 16:42:36 +0700

    modified cart components and implement contents inside it

--- src/components/Meals/Item/index.js
+++ src/components/Meals/Item/index.js
@@ -1,8 +1,20 @@
+import { useContext } from 'react';
+import CartContext from '../../../store/Cart/CartContext';
 import MealItemForm from './Form';
 import styles from './style.module.css';
 
 const MealsItem = (props) => {
+  const cartCtx = useContext(CartContext);
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
 
   return (
     <li className={styles.meal}>
@@ -12,7 +24,7 @@ const MealsItem = (props) => {
         <div className={styles.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm id={props.id} />
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
--- src/components/UI/Cart/index.js
+++ src/components/UI/Cart/index.js
@@ -1,14 +1,29 @@
+import { useContext } from 'react';
+import CartContext from '../../../store/Cart/CartContext';
+import CartItem from '../../Cart/CartItem';
 import Modal from '../Modal';
 import styles from './style.module.css';
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemAddHandler = (item) => {};
+  const cartItemRemoveHandler = (item) => {};
+
   const cartItems = (
     <ul className={styles['cart-items']}>
-      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map(
-        (item) => (
-          <li key={item.id}>{item.name}</li>
-        ),
-      )}
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={`${item.id} ${Math.random()}`}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onAdd={cartItemAddHandler.bind(null, item.id)}
+          onRemove={cartItemRemoveHandler.bind(null, item)}
+        />
+      ))}
     </ul>
   );
 
@@ -17,7 +32,7 @@ const Cart = (props) => {
       {cartItems}
       <div className={styles.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={styles.actions}>
         <button
@@ -26,7 +41,7 @@ const Cart = (props) => {
         >
           Close
         </button>
-        <button className={styles.button}>Order</button>
+        {hasItems && <button className={styles.button}>Order</button>}
       </div>
     </Modal>
   );
